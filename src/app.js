fd25955d9a26b36eda334c9b8dd4a11391852a4c
MarkDrastrup <mark.drastrup@gmail.com>
2019-07-27 09:59:23 +0900

    Made basic CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 require('dotenv').config()
 
@@ -8,4 +8,60 @@ app.get("/", (req, res) => {
 
 app.listen(4000, () => {
   console.log("App is running on port 4000");
-});
+}); */
+
+var express = require('express');
+require('dotenv').config()
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+// GraphQL schema
+var schema = buildSchema(`
+    type User {
+      id: ID
+      username: String
+      created_at: String
+    }
+    type Query {
+      getUser(username: String): [User]
+      getAllUsers: [User]
+    }
+    type Mutation {
+      createUser(username: String): [User]
+      updateUser(id: Int, username: String, update: String): [User]
+      deleteUser(username: String): [User]
+    }
+`);
+// Root resolver
+var root = {
+  getUser: async ({ username }) => {
+    const user = await database("users").where({ username: username });
+    return user;
+  },
+  getAllUsers: async () => {
+    const users = await database("users").select();
+    return users;
+  },
+  createUser: async ({ username }) => {
+    const newUser = await database("users").insert({ username: username }).returning("*").into("users");
+    return newUser;
+  },
+  updateUser: async ({ id, username, update }) => {
+    const updatedUser = await database("users").where({ id: id }).orWhere({ username: username }).update({ username: update }).returning("*").into("users");
+    return updatedUser;
+  },
+  deleteUser: async ({ username }) => {
+    const deletedUser = await database("users").where({ username: username }).del().returning("*").into("users");
+    return deletedUser;
+  }
+};
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
