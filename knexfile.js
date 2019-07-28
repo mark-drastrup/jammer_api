f70bd7920c3824fd1101ae5844fd954019744b4e
MarkDrastrup <mark.drastrup@gmail.com>
2019-07-28 22:10:55 +0900

    References Heroku database url config var

--- knexfile.js
+++ knexfile.js
@@ -36,13 +36,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: {
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      user: process.env.DBUSER,
-      password: process.env.PGPASSWORD,
-      ssl: true
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
